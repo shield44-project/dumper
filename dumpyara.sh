247cc2055bc181e3978f653955a03f3277f3c725
shield44 <shield44@pixelexperience.org>
2022-07-21 21:51:52 +0530

    Update GitHub username and email

--- dumpyara.sh
+++ dumpyara.sh
@@ -164,10 +164,10 @@ if [[ -n $GIT_OAUTH_TOKEN ]]; then
     curl --silent --fail "https://raw.githubusercontent.com/$ORG/$repo/$branch/all_files.txt" 2> /dev/null && echo "Firmware already dumped!" && exit 1
     git init
     if [[ -z "$(git config --get user.email)" ]]; then
-        git config user.email androygaming@protonmail.com
+        git config user.email motoecondorxt1022@gmail.com
     fi
     if [[ -z "$(git config --get user.name)" ]]; then
-        git config user.name SuperCosmicBeing
+        git config user.name shield44
     fi
     git checkout -b "$branch"
     find . -size +97M -printf '%P\n' -o -name "*sensetime*" -printf '%P\n' -o -name "*.lic" -printf '%P\n' >| .gitignore
